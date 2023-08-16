5470af36be5262930f4bd57651ccddf0d3763f8d
mm-masahiro <hand5.mm9.mzh@gmail.com>
2023-08-16 22:24:52 +0900

    fix run method

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,58 +1,60 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use futures::executor::block_on;
 use sea_orm::{ConnectionTrait, Database, DbBackend, DbErr, Statement};
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 const DATABASE_URL: &str = "mysql://root:password@localhost:3306";
 const DB_NAME: &str = "menu_and_shopping_list";
 
-async fn run () -> Result<(), DbErr> {
-	let db = Database::connect(DATABASE_URL).await?;
+async fn run() -> Result<(), DbErr> {
+    let db = Database::connect(DATABASE_URL).await?;
 
-	let db = &match db.get_database_backend() {
-		DbBackend::MySql => {
-			db.execute(Statement::from_string(db.get_database_backend(), format!("CREATE DATABASE IF NOT EXISTS `{}`;", DB_NAME),)).await?;
+    let db = &match db.get_database_backend() {
+        DbBackend::MySql => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE IF NOT EXISTS `{}`;", DB_NAME),
+            ))
+            .await?;
 
-			let url = format!("{}/{}", DATABASE_URL, DB_NAME);
-			Database::connect(&url).await?
-		}
+            let url = format!("{}/{}", DATABASE_URL, DB_NAME);
+            Database::connect(&url).await?
+        }
 
-		DbBackend::Postgres => {
-			db.execute(Statement::from_string(
-				db.get_database_backend(),
-				format!("DROP DATABASE IF EXISTS \"{}\";", DB_NAME),
-			)).await?;
-			db.execute(Statement::from_string(
-				db.get_database_backend(),
-				format!("CREATE DATABASE \"{}\";", DB_NAME),
-			)).
-			await?;
+        DbBackend::Postgres => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("DROP DATABASE IF EXISTS \"{}\";", DB_NAME),
+            ))
+            .await?;
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE \"{}\";", DB_NAME),
+            ))
+            .await?;
 
-			let url = format!("{}/{}", DATABASE_URL, DB_NAME);
-			Database::connect(&url).await?
-		}
+            let url = format!("{}/{}", DATABASE_URL, DB_NAME);
+            Database::connect(&url).await?
+        }
 
-			DbBackend::Sqlite => db,
-	};
+        DbBackend::Sqlite => db,
+    };
 
-	Ok(())
+    Ok(())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-	HttpServer::new(|| {
-		App::new()
-				.service(hello)
-	})
-	.bind(("127.0.0.1", 8080))?
-	.run()
-	.await
+    if let Err(err) = block_on(run()) {
+        panic!("{}", err);
+    }
+
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
 
 #[get("/")]
-async fn hello () -> impl Responder {
-	if let Err(err) = block_on(run()) {
-		panic!("{}", err);
-	}
-
-  HttpResponse::Ok().body("Hello World!")
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
 }
