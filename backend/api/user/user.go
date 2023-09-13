852b5db49b0ad744b1cb53215b4ae746a0fe0c5e
mm-masahiro <hand5.mm9.mzh@gmail.com>
2023-09-13 22:11:07 +0900

    リクエストを受け取れるようにした

--- backend/api/user/user.go
+++ backend/api/user/user.go
@@ -5,6 +5,14 @@ import (
 	"net/http"
 )
 
+type CreateRequest struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 func Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello from MyHandler!")
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	fmt.Println(string(body))
 }
