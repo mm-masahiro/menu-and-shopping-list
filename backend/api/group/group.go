95b3a852a169445bc67fce638607ca929665bf62
mm-masahiro <hand5.mm9.mzh@gmail.com>
2023-10-25 23:50:09 +0900

    groupの作成ができるようにした

--- backend/api/group/group.go
+++ backend/api/group/group.go
@@ -13,24 +13,21 @@ import (
 
 type CreateRequest struct {
 	gorm.Model
-	Name    string `json:"name"`
-	Email   string `json:"email"`
-	GroupId int    `json:"groupId"`
-	Group   data.Group
+	ParentUserId int `json:"parentUserId"`
 }
 
 func Create(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := io.ReadAll(r.Body)
 
-	var user data.User
+	var group data.Group
 
-	if err := json.Unmarshal(reqBody, &user); err != nil {
+	if err := json.Unmarshal(reqBody, &group); err != nil {
 		log.Fatal(err)
 	}
 
-	data.CreateUser((&user))
+	data.CreateGroup((&group))
 
-	responseBody, err := json.Marshal(user)
+	responseBody, err := json.Marshal(group)
 	if err != nil {
 		log.Fatal(err)
 	}
--- backend/model/group/group.go
+++ backend/model/group/group.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 
 	data "backend/model"
@@ -9,6 +11,9 @@ import (
 var db *gorm.DB
 
 func CreateGroup(group *data.Group) error {
+	// nilになってる
+	fmt.Println(db)
+
 	if err := db.Create(&group).Error; err != nil {
 		return err
 	}
